5885d02f7a86e50261e5748d6befe8580224724a
tangjianhua <157618647@qq.com>
2020-12-16 20:49:23 +0800

    第一个go程序

--- k8s/main.go
+++ k8s/main.go
@@ -3,17 +3,39 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"log"
+	"net"
 )
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
+		var ips string
+		if getIp() == "192.168.63.133" {
+			ips = "main"
+		} else {
+			ips = "work1"
+		}
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": ips,
 		})
 	})
-	err := r.Run(":80") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	err := r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func getIp() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return ""
+	}
+	for _, value := range addrs {
+		if ipnet, ok := value.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
 }
